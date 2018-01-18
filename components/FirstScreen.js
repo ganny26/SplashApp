90a7c3ff3133be2e2d5450828683f061ae52c8a5
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2018-01-18 22:57:47 +0530

    first screen updated

--- components/FirstScreen.js
+++ components/FirstScreen.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import {Text, View,Button} from 'react-native';
+import { Text, View, Button,StyleSheet } from 'react-native';
+import { Card, ListItem, Button } from 'react-native-elements';
 
+class FirstScreen extends React.Component {
+    constructor(props){
+        super(props);
+        console.log('first screen',this.props)
+    }
+    static navigationOptions = ({ navigation }) => ({
+        title: 'First',
+    });
+    render() {
 
-class FirstScreen extends React.Component{
-        render(){
-            const {navigate} = this.props.navigation;
-            return(
-                <View>
-                    <Text>This is first screen</Text>
-                    <Button title="Press Me" onPress={()=>navigate("Second",{'name':'Hello'})}>
-                    </Button>
-                </View>
-            )
-        }
+        const { navigate } = this.props.navigation;
+        return (
+            <View style={styles.wrapper}>
+               <Card title="ingredient">
+               <View>
+                   <Text>Card 1</Text>
+                   
+                   </View>
+               </Card>
+
+            </View>
+        )
+    }
 }
+
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }
+})
 
 export default FirstScreen;
