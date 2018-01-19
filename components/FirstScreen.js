86766976e4d42547f99e4d99791a885778bbd3d6
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2018-01-19 16:00:02 +0530

    landing view updated

--- components/FirstScreen.js
+++ components/FirstScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Text, View, Button,StyleSheet } from 'react-native';
-import { Card, ListItem, Button } from 'react-native-elements';
-
+import { Text, View, StyleSheet, StatusBar, ScrollView } from 'react-native';
+import { Card, ListItem, Button, SearchBar } from 'react-native-elements';
+import * as Progress from 'react-native-progress';
+import KnowledgeComponent from './KnowledgeComponent';
 class FirstScreen extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        console.log('first screen',this.props)
+        console.log('first screen', this.props)
     }
     static navigationOptions = ({ navigation }) => ({
         title: 'First',
@@ -14,26 +15,51 @@ class FirstScreen extends React.Component {
 
         const { navigate } = this.props.navigation;
         return (
-            <View style={styles.wrapper}>
-               <Card title="ingredient">
-               <View>
-                   <Text>Card 1</Text>
-                   
-                   </View>
-               </Card>
+            <ScrollView style={{
+                flex: 1, alignSelf: 'stretch'
+            }}>
+                <View>
+                    <View style={styles.SearhbarStyle}>
+                        <SearchBar
+                            lightTheme
+                            round
+                            placeholder='Search Recipes' />
+                    </View>
+                    <View>
+                        <Card title="Recipe 1">
+                            <KnowledgeComponent fieldLevel={0.3} fieldText="Carbs" fieldValue="32g | 23%" />
+                            <KnowledgeComponent fieldLevel={0.5} fieldText="Protein" fieldValue="22g | 50%" />
+                            <KnowledgeComponent fieldLevel={0.8} fieldText="Fat" fieldValue="188g | 80%" />
+                        </Card>
+                        <Card title="Recipe 2">
+                            <KnowledgeComponent fieldLevel={0.3} fieldText="Carbs" fieldValue="32g | 23%" />
+                            <KnowledgeComponent fieldLevel={0.5} fieldText="Protein" fieldValue="22g | 50%" />
+                            <KnowledgeComponent fieldLevel={0.8} fieldText="Fat" fieldValue="188g | 80%" />
+                        </Card>
+                        <Card title="Recipe 3">
+                            <KnowledgeComponent fieldLevel={0.3} fieldText="Carbs" fieldValue="32g | 23%" />
+                            <KnowledgeComponent fieldLevel={0.5} fieldText="Protein" fieldValue="22g | 50%" />
+                            <KnowledgeComponent fieldLevel={0.8} fieldText="Fat" fieldValue="188g | 80%" />
+                        </Card>
+                    </View>
+                </View>
+            </ScrollView>
 
-            </View>
         )
     }
 }
 
 
 const styles = StyleSheet.create({
+    SearhbarStyle: {
+        marginTop: 30
+    },
     wrapper: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-      }
+        flex: 1
+    },
+    progress: {
+        margin: 10,
+    }
 })
 
 export default FirstScreen;
